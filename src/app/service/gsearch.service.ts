3f1ea5ce49e30ceb60fb1b65f71abb1b52cc13e3
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-20 09:24:44 +0300

    Add authorization

--- src/app/service/gsearch.service.ts
+++ src/app/service/gsearch.service.ts
@@ -1,10 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { resolve } from 'dns';
+import { user } from '../user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GsearchService {
+  user!:user;
 
   constructor(private http:HttpClient) { }
+
+  findUser(ghubname:string){
+    const promise = new Promise<void>((resolve,reject)=>{
+      this.http.get<user>('${environment.base_url}${ghubname}',{
+        headers:{
+          Authorization: 'token ${environment.access_token}'
+        }
+      }).subscribe
+    })
+  }
 }
